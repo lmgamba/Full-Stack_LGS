0b26708b5cc18ac1aa95d8d6a58638a1f0980439
Laura Gamba <lm.gamba@uniandes.edu.co>
2025-11-23 21:59:43 +0100

    adding python folder

---
+++ ejercicios_python/rick_and_morty/main.py
@@ -0,0 +1,33 @@
+    """###########################################
+
+4. EJERCICIO: Trabajar con la API de Rick & Morty usando requests
+Construir un pequeño programa en Python que consulte la API pública de Rick & Morty y realice varias búsquedas y filtrados utilizando la librería requests.
+
+sando la API pública de Rick and Morty:
+
+URL base:
+https://rickandmortyapi.com/api/character
+
+Debes crear un script en Python que:
+
+Debes crear un script en Python que:
+
+1️⃣ Obtenga 20 primeros personajes de la API
+
+Guarda el resultado final en una lista llamada personajes.
+
+2️⃣ Imprima:
+
+El número total de personajes
+Los primeros n personajes siendo n un numero pedido al usuario
+
+
+3️⃣ Filtrar personajes raza
+
+Crear una lista con todos los personajes cuya especie sea "Human". Siendo Human algo pedido por pantalla
+
+4️⃣ Buscar personajes por estado
+
+Pide al usuario un estado (Alive, Dead, unknown) y muestra:
+El número total de personajes con ese estado.
+    """
