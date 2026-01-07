9e926dfbcb8f3ef3f975c90c5fae38f69e9945ed
Laura Gamba <lm.gamba@uniandes.edu.co>
2026-01-07 16:55:12 +0100

    CRUD: falta filtro-quantity

--- 3_api_mysql/controllers/product_controller.py
+++ 3_api_mysql/controllers/product_controller.py
@@ -5,8 +5,9 @@ from models.product_model import *
 
 
 #AQUI SE CONECTA A LA DATABASE
-
-async def obtener_producto_by_id(product_id):
+#################################################
+################## READ ######################
+async def obtener_producto_by_id(product_id: int):
     try:
         conn= await get_connection()
         #abrir sql para lanzar queries
@@ -21,9 +22,9 @@ async def obtener_producto_by_id(product_id):
         conn.close()
         
         
-#obtener productos entre un rango de precioes
+#### obtener productos entre un rango de precios #####
 
-async def obtener_productos_by_price(pricemin,pricemax):
+async def obtener_productos_by_price(pricemin: int,pricemax: int):
     
     if pricemin > pricemax:
         raise   HTTPException(status_code=400, detail="El precio min no puede ser mayor al maxima")
@@ -45,7 +46,46 @@ async def obtener_productos_by_price(pricemin,pricemax):
         conn.close()
         
         
-        
+##### obtener productos entre un rango de precioesbuscador ##
+async def obtener_productos_by_title(in_title: str):
+
+    try:
+        conn= await get_connection()
+        #abrir sql para lanzar queries
+        async with conn.cursor(aio.DictCursor) as cursor:
+            #se lanza la consulta
+            await cursor.execute("SELECT * FROM products WHERE products.title LIKE %s ",(f'%{in_title}%',))
+            res= await cursor.fetchall()
+            if len(res) != 0:
+                return res
+            else:
+                raise   HTTPException(status_code=404, detail="No se encontraron productos con ese titulo")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error:{str(e)}")
+    finally:
+        conn.close()
+ 
+ ########STATUS############       
+async def obtener_productos_by_status(product_status:int):
+    try:
+        conn= await get_connection()
+        #abrir sql para lanzar queries
+        async with conn.cursor(aio.DictCursor) as cursor:
+            #se lanza la consulta
+            await cursor.execute("SELECT * FROM products WHERE status=%s ",(product_status,))
+            res= await cursor.fetchall()
+            if len(res) != 0:
+                return res
+            else:
+                raise   HTTPException(status_code=404, detail="No se encontraron productos con ese status")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error:{str(e)}")
+    finally:
+        conn.close()
+
+
+#################################################    
+################## CREATE ######################
 async def registrar(product: productCreate):
     try:
         conn= await get_connection()
@@ -68,7 +108,9 @@ async def registrar(product: productCreate):
     ################# HASTA AQUI OKK#########
     
     
-    #TODO: ADAPTAR borrar y update    
+    #TODO: ADAPTAR borrar y update   
+#################################################
+################## DELETE ###################### 
 async def borrar_producto_by_id(product_id:int):
     product= await obtener_producto_by_id(product_id)
     if product is not None:
@@ -85,11 +127,11 @@ async def borrar_producto_by_id(product_id:int):
         finally:
             conn.close()
     else:
-        raise HTTPException(status_code=404, detail="El ususario que intentas eliminar no existe")
+        raise HTTPException(status_code=404, detail="El producto que intentas eliminar no existe")
 
-
-
-async def actualizar_producto_by_id(product_id, product: product):
+#################################################
+################## UPDATE ######################
+async def actualizar_producto_by_id(product_id: int, product: product):
     #para verificar que el producto enviado en el formulario es realmente el mismo que se busca actualizar
     if product.id != product_id:
         raise HTTPException(status_code= 400, detail= "El id no coincide")
@@ -104,7 +146,7 @@ async def actualizar_producto_by_id(product_id, product: product):
         #abrir sql para lanzar queries
         async with conn.cursor(aio.DictCursor) as cursor:
             #se lanza la consulta
-            await cursor.execute("UPDATE products SET name=%s, surname=%s, price=%s, mail=%s, status=%s, password=%s, rol=%s WHERE id=%s", ( product.name, product.surname, product.price, product.mail, product.status, product.password, product.rol,product.id))
+            await cursor.execute("UPDATE products SET title=%s, price=%s, quantity=%s, status=%s WHERE id=%s", ( product.title, product.price, product.quantity, product.status,product.id))
 
             await conn.commit() #el commit no devuelve nada, solo hace el registro
             producto = await obtener_producto_by_id(product_id)
@@ -114,3 +156,6 @@ async def actualizar_producto_by_id(product_id, product: product):
     finally:
         conn.close()
     
+
+    
+        
--- 3_api_mysql/routes/product_routes.py
+++ 3_api_mysql/routes/product_routes.py
@@ -6,30 +6,48 @@ from models.product_model import *
 
 router = APIRouter()
 
-# obtener producto por id http://localhost:8000/products/
+################## READ ######################
+# obtener producto por id http://localhost:8000/products/ 
 
 @router.get('/{product_id}', status_code= 200)
 async def get_products_by_id(product_id: str):
     #llama al controlador a la funcion obtener_productos por id -->>> RECORDAR pasar id a int
     return await product_controller.obtener_producto_by_id(int(product_id))
 
-# obtener producto por precioss http://localhost:8000/products/
+## STATUS 
+@router.get('/filter/status', status_code= 200)
+async def get_products_by_price(product_status: int):
+    return await product_controller.obtener_productos_by_status(product_status)
 
-
+## PRECIO ok  # obtener producto por precioss http://localhost:8000/products/filter/precio?pricemin=20&pricemax=70
 @router.get('/filter/precio', status_code= 200)
 async def get_products_by_price(pricemin: int,pricemax: int):
     return await product_controller.obtener_productos_by_price(pricemin,pricemax)
 
+## TITULO
+@router.get('/filter/title', status_code= 200)
+async def get_products_by_price(in_title: str):
+    return await product_controller.obtener_productos_by_title(in_title)
+
+
+
+
+################## CREATE ######################
+
+## POST ok
 @router.post('/register', status_code= 201)
 async def register(product: productCreate):
     return await product_controller.registrar(product)
+
+
+################## DELETE ######################
 
 @router.delete('/{product_id}', status_code= 200)
 async def delete_product_by_id(product_id: str):
     return await product_controller.borrar_producto_by_id(int(product_id))
 
 
-# update  con PUT
+################## UPDATE ######################
 @router.put('/{product_id}', status_code= 200)
 async def update_product_by_id(product_id: str, product: product):
     return await product_controller.actualizar_producto_by_id(int(product_id), product)
